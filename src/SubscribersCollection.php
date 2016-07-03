9e4880a58dd3123bee1b21c5cbc8eb81aed97a77
Miguel <miguel.mailbox@gmail.com>
2016-07-03 10:08:00 +0200

    New class for subscribers list.

---
+++ src/SubscribersCollection.php
@@ -0,0 +1,50 @@
+<?php
+
+
+namespace H4D\Patterns;
+
+
+use H4D\Patterns\Interfaces\SubscriberInterface;
+use SplObjectStorage;
+
+class SubscribersCollection extends SplObjectStorage
+{
+    /**
+     * @param SubscriberInterface $object
+     * @param mixed $data
+     */
+    public function attach(SubscriberInterface $object, $data = null)
+    {
+        parent::attach($object, $data);
+    }
+
+    /**
+     * @param SubscriberInterface $object
+     */
+    public function detach(SubscriberInterface $object)
+    {
+        parent::detach($object);
+    }
+
+    /**
+     * @param SubscriberInterface $object
+     *
+     * @return bool
+     */
+    public function contains(SubscriberInterface $object)
+    {
+        return parent::contains($object);
+    }
+
+    /**
+     * @param SubscriberInterface $object
+     *
+     * @return string
+     */
+    public function getHash(SubscriberInterface  $object)
+    {
+        return parent::getHash($object);
+    }
+
+
+}
