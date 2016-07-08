5acf8560e1418d18ae421b67ebaac7015ef8171d
Miguel <miguel.mailbox@gmail.com>
2016-07-08 12:34:15 +0200

    Update test due to interface changes.

--- tests/unit/Events/AbstractEventTest.php
+++ tests/unit/Events/AbstractEventTest.php
@@ -15,21 +15,14 @@ class AbstractEventTest extends \PHPUnit_Framework_TestCase
         $msg = 'Test event';
         $event = new MyEventForTests($msg, $context);
         $this->assertTrue($event instanceof EventInterface);
-        $this->assertEquals($context, $event->getContext());
+        $this->assertEquals($context, $event->getContext()->getAll());
         $this->assertEquals($msg, $event->getMessage());
-        $this->assertEquals('A', $event->getContextValue('a'));
-        $this->assertEquals('B', $event->getContextValue('b'));
-        $this->assertTrue($event->hasContextKey('a'));
-        $this->assertTrue($event->hasContextKey('b'));
-        $this->assertFalse($event->hasContextKey('c'));
+        $this->assertEquals('A', $event->getContext()->get('a'));
+        $this->assertEquals('B', $event->getContext()->get('b'));
+        $this->assertTrue($event->getContext()->has('a'));
+        $this->assertTrue($event->getContext()->has('b'));
+        $this->assertFalse($event->getContext()->has('c'));
         $this->assertTrue(is_int($event->getTimestamp()));
-        $this->assertArrayHasKey('timestamp', $event->toArray());
-        $this->assertArrayHasKey('message', $event->toArray());
-        $this->assertArrayHasKey('context', $event->toArray());
-        $this->assertEquals($msg, $event->toArray()['message']);
-        $this->assertEquals($context, $event->toArray()['context']);
-        $this->assertEquals($event->getTimestamp(), $event->toArray()['timestamp']);
-
     }
 
     /**
